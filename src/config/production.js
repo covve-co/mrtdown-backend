ef0c60ca3baf12bff6a468964a9d62692670cdcf
Ravern Koh <ravern.koh.dev@gmail.com>
2017-10-29 10:52:35 +0800

    Made the production config reflect the dev one

--- src/config/production.js
+++ src/config/production.js
@@ -1,25 +1,32 @@
 module.exports = {
   port: process.env.PORT,
   host: 'helloworld.com',
+
   logLevel: 'info',
+
+  mongodb: {
+    url: process.env.MONGODB_URL,
+  },
+
   firebase: {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY,
     databaseUrl: process.env.FIREBASE_DATABASE_URL
   },
+
+  reddit: {
+    userAgent: 'MRTDownBot',
+    clientId: process.env.REDDIT_CLIENT_ID,
+    clientSecret: process.env.REDDIT_CLIENT_SECRET,
+    username: process.env.REDDIT_USERNAME,
+    password: process.env.REDDIT_PASSWORD,
+  },
+
   twitter: {
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
     accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
     accessTokenSecret: process.env.TWITTER_TOKEN_SECRET,
   }
-
-  lineNames: {
-    EWL: 'East-West Line',
-    NSL: 'North-South Line',
-    CCL: 'Circle Line',
-    NEL: 'North-East Line',
-    DTL: 'Downtown Line',
-  },
 };
